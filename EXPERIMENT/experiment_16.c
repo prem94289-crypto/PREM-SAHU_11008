2ac9085fbd7cd5f4c68036c4879fefad389d7210
prem94289-crypto <prem94289@gmail.com>
2025-12-31 22:00:19 +0530

    experiment

---
+++ EXPERIMENT/experiment_16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a[5], i, j, temp;
+
+    printf("Enter 5 elements:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Bubble Sort logic
+    for (i = 0; i < 5 - 1; i++) {
+        for (j = 0; j < 5 - 1 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
---
+++ EXPERIMENT/experiment_17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3];
+    int i, j;
+
+    printf("Enter elements of 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Matrix elements are:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
